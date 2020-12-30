377386f8f1c0bb71c7f4bb21f9bf6a0645e48915
ZackBNT <zakaria.bnnt@gmail.com>
2020-12-30 15:06:54 +0100

    Premiere version interface graphique

---
+++ src/TerraGenesis/Conditions.java
@@ -0,0 +1,195 @@
+package utbm.TerraGenesis;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+public class Conditions extends Home{
+
+	JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(new NimbusLookAndFeel());
+					Conditions window = new Conditions();
+					window.frame.setVisible(true);
+					window.frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Conditions() {
+		initialize();
+		mu.stop();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1109, 717);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton_3 = new JButton("Back");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_3.setIcon(new ImageIcon(".\\Images\\Go-back-icon-1.png"));
+		btnNewButton_3.addActionListener(bHandler);
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+
+			}
+		});
+		btnNewButton_3.setBounds(10, 638, 103, 32);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mars 2035");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Showcard Gothic", Font.BOLD | Font.ITALIC, 26));
+		lblNewLabel_2.setBounds(468, 10, 168, 39);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Temperature :");
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3.setBounds(65, 166, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Pression :");
+		lblNewLabel_3_1.setForeground(Color.WHITE);
+		lblNewLabel_3_1.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3_1.setBounds(65, 231, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_1_1 = new JLabel("Gaz :");
+		lblNewLabel_3_1_1.setForeground(Color.WHITE);
+		lblNewLabel_3_1_1.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3_1_1.setBounds(65, 506, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3_1_1);
+		
+		
+		JLabel lblNewLabel_3_1_2 = new JLabel("Oxygen :");
+		lblNewLabel_3_1_2.setForeground(Color.WHITE);
+		lblNewLabel_3_1_2.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3_1_2.setBounds(65, 368, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3_1_2);
+		
+		JLabel lblNewLabel_3_1_2_1 = new JLabel("Water :");
+		lblNewLabel_3_1_2_1.setForeground(Color.WHITE);
+		lblNewLabel_3_1_2_1.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3_1_2_1.setBounds(65, 437, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3_1_2_1);
+	
+		JLabel lblNewLabel_3_1_2_2 = new JLabel("Biomass :");
+		lblNewLabel_3_1_2_2.setForeground(Color.WHITE);
+		lblNewLabel_3_1_2_2.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_3_1_2_2.setBounds(65, 298, 233, 32);
+		frame.getContentPane().add(lblNewLabel_3_1_2_2);
+		
+		textField = new JTextField("5000D");
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField.setEditable(false);
+		textField.setHorizontalAlignment(JTextField.CENTER);
+		textField.setBounds(255, 234, 125, 32);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField("332147W");
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField_1.setEditable(false);
+		textField_1.setHorizontalAlignment(JTextField.CENTER);
+		textField_1.setColumns(10);
+		textField_1.setBounds(255, 301, 125, 32);
+		frame.getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField("100P");
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField_2.setEditable(false);
+		textField_2.setHorizontalAlignment(JTextField.CENTER);
+		textField_2.setColumns(10);
+		textField_2.setBounds(255, 371, 125, 32);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField("94560ml");
+		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField_3.setEditable(false);
+		textField_3.setHorizontalAlignment(JTextField.CENTER);
+		textField_3.setColumns(10);
+		textField_3.setBounds(255, 440, 125, 32);
+		frame.getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField("600°C");
+		textField_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField_4.setEditable(false);
+		textField_4.setHorizontalAlignment(JTextField.CENTER);
+		textField_4.setColumns(10);
+		textField_4.setBounds(255, 169, 125, 32);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField("10000K");
+		textField_5.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		textField_5.setEditable(false);
+		textField_5.setHorizontalAlignment(JTextField.CENTER);
+		textField_5.setColumns(10);
+		textField_5.setBounds(255, 509, 125, 32);
+		frame.getContentPane().add(textField_5);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(878, 27, 207, 11);
+		frame.getContentPane().add(progressBar);
+	
+		JLabel lblNewLabel_6 = new JLabel("Mission");
+		lblNewLabel_6.setForeground(Color.WHITE);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_6.setBounds(955, 12, 59, 11);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_4 = new JLabel("100000$");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setIcon(new ImageIcon(".\\Images\\coin-us-dollar-icon-1.png"));
+		lblNewLabel_4.setBounds(38, 10, 123, 28);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new ImageIcon(".\\Images\\terree.png"));
+		lblNewLabel.setBounds(570, 79, 515, 523);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel();
+		lblNewLabel_1.setIcon(new ImageIcon(".\\Images\\lunee.jpg"));
+		lblNewLabel_1.setBounds(0, 0, 1106, 680);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+	}
+}
---
+++ src/TerraGenesis/Home.java
@@ -0,0 +1,270 @@
+package utbm.TerraGenesis;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.JProgressBar;
+
+public class Home {
+
+	JFrame frame;
+	ButtonHandler bHandler = new ButtonHandler();
+	String clickSound, tavernMusic;
+	SoundEffect se = new SoundEffect();
+	Music mu = new Music();
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				
+				try {
+					UIManager.setLookAndFeel(new NimbusLookAndFeel());
+					Home window = new Home();
+					window.frame.setVisible(true);
+					window.frame.setLocationRelativeTo(null);
+				
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			
+
+			}
+			
+		});
+		
+	}
+
+	
+	public Home() {
+		
+		
+			initialize();
+			mu.setFile(tavernMusic);
+			mu.play();	
+	}
+	
+	private void initialize() {
+		
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1109, 717);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Build the city");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.addActionListener(bHandler);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				City city = new City();
+				city.frame.setVisible(true);
+			}	
+		});
+		btnNewButton.setBounds(65, 291, 175, 62);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Add Animals");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_1.addActionListener(bHandler);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Animals animals = new Animals();
+				animals.frame.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(65, 402, 175, 62);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Add Vegetals");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_2.addActionListener(bHandler);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Vegetals vegetals = new Vegetals();
+				vegetals.frame.setVisible(true);
+			}
+		});
+		btnNewButton_2.setBounds(315, 402, 175, 62);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_5 = new JButton("Add Humans");
+		btnNewButton_5.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_5.addActionListener(bHandler);
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Humans humans = new Humans();
+				humans.frame.setVisible(true);
+			}
+		});
+		btnNewButton_5.setBounds(315, 291, 175, 62);
+		frame.getContentPane().add(btnNewButton_5);
+		
+		
+		JButton btnNewButton_3 = new JButton("Buy Materials");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_3.addActionListener(bHandler);
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Materials materials = new Materials();
+				materials.frame.setVisible(true);
+			}
+		});
+		btnNewButton_3.setBounds(10, 631, 135, 39);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_3_1 = new JButton("");
+		btnNewButton_3_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_3_1.addActionListener(bHandler);
+		btnNewButton_3_1.setBounds(208, 631, 135, 39);
+		frame.getContentPane().add(btnNewButton_3_1);
+		
+		JButton btnNewButton_3_1_1 = new JButton("View Conditions");
+		btnNewButton_3_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_3_1_1.addActionListener(bHandler);
+		btnNewButton_3_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Conditions conditions = new Conditions();
+				conditions.frame.setVisible(true);
+			}
+		});
+		btnNewButton_3_1_1.setBounds(403, 631, 135, 39);
+		frame.getContentPane().add(btnNewButton_3_1_1);
+		
+		JButton btnNewButton_4 = new JButton("Exit");
+		btnNewButton_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_4.setIcon(new ImageIcon(".\\Images\\Close-2-icon.png"));
+		btnNewButton_4.addActionListener(bHandler);
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		btnNewButton_4.setBounds(992, 640, 93, 30);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mars 2035");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Showcard Gothic", Font.BOLD | Font.ITALIC, 26));
+		lblNewLabel_2.setBounds(468, 10, 168, 39);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(878, 27, 207, 11);
+		frame.getContentPane().add(progressBar);
+	
+		JLabel lblNewLabel_3 = new JLabel("Mission");
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_3.setBounds(955, 12, 59, 11);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("100000$");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setIcon(new ImageIcon(".\\Images\\coin-us-dollar-icon-1.png"));
+		lblNewLabel_4.setBounds(38, 10, 123, 28);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon(".\\Images\\Webp.net-resizeimage.png"));
+		lblNewLabel_5.setBounds(208, 79, 146, 156);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new ImageIcon(".//Images//terree.png"));
+		lblNewLabel.setBounds(570, 79, 515, 523);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel();
+		lblNewLabel_1.setIcon(new ImageIcon(".//Images//lunee.jpg"));
+		lblNewLabel_1.setBounds(0, 0, 1106, 680);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		clickSound = ".//Sounds//button.wav";
+		tavernMusic = ".//Sounds//backMusic.wav";
+
+	}
+	
+	public class SoundEffect{
+		
+		Clip clip;
+		
+		public void setFile(String soundFileName) {
+			
+			try {
+				File file = new File(soundFileName);
+				AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+			}
+			catch(Exception e){
+				
+			}
+		}
+		
+		public void play() {
+			
+			clip.setFramePosition(0);
+			clip.start();
+		}
+	}
+	
+	public class ButtonHandler implements ActionListener {
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			se.setFile(clickSound);
+			se.play();
+		}
+	}
+
+public class Music{
+		
+		Clip clip;
+		
+		public void setFile(String soundFileName) {
+			
+			try {
+				File file = new File(soundFileName);
+				AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+			}
+			catch(Exception e){
+				
+			}
+		}
+		
+		public void play() {
+			
+			clip.setFramePosition(0);
+			clip.start();
+		}
+		
+		public void loop(){
+			
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+		
+		public void stop(){
+			
+			clip.stop();
+			clip.close();
+		}
+	} 
+}
