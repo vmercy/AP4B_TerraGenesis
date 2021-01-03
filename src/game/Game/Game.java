965275cfb987e1e050361ce9386e6b0b016a6222
AUDRAN <AUDRAN@DESKTOP-T93J1J9.home>
2021-01-03 12:22:51 +0100

    update

--- src/game/Game/Game.java
+++ src/game/Game/Game.java
@@ -1,8 +1,6 @@
 
 package Game;
 
-import java.util.*;
-import java.time.*;
 
 public class Game {
     
@@ -99,22 +97,21 @@ public class Game {
     
     public void AnimalSpeciesIntroduced(Animal animal_p)
     {
-    	double newCarbonDioxide;
-    	this.carbonDioxide += animal_p.ca
+    	double addCarbonDioxide;
+    	addCarbonDioxide = animal_p.getCarbonDioxideProduced();
+    	this.carbonDioxide += addCarbonDioxide;
     }
     	
-    
-    
     /**
      * @param Vegetal vegetal_p
      * @brief Lorsque qu'un végétal est instauré avec succès sur la planète, la jauge d'O2 monte
      */
     
-    public void vegetalSpeciesIntroduced(Animal animal_p)
+    public void vegetalSpeciesIntroduced(Vegetal vegetal_p)
     {
-    	double newOxygen;
-    	newOxygen = this.oxygen + vegetal_p.getOxygenGiven();
-    	this.setOxygen(newOxygen);
+    	double addOxygen;
+    	addOxygen = vegetal_p.getOxygenGiven();
+    	this.oxygen += addOxygen;
     }
     
     /**
@@ -123,24 +120,60 @@ public class Game {
      * 		  alors la jauge d'atmosphère passe à 10%
      */
     
-    public void atmosphereFirstAdvanced()
+    public void atmosphereStateUpdate()
     {
-    	if(this.getCarbonDioxide()>=10 && this.getOxygen()>=10)
+    	if(this.carbonDioxide>=10 && this.carbonDioxide<20 && this.oxygen>=10 && this.oxygen<20)
     	{
-    		this.setPression(10);
+    		this.atmosphere = 10;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 10% !");
+    	}
+    	if(this.carbonDioxide>=20 && this.carbonDioxide<30 && this.oxygen>=20 && this.oxygen<30)
+    	{
+    		this.atmosphere = 20;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 20% !");
+    	}
+    	if(this.carbonDioxide>=30 && this.carbonDioxide<40 && this.oxygen>=30 && this.oxygen<40)
+    	{
+    		this.atmosphere = 30;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 30% !");
+    	}
+    	if(this.carbonDioxide>=40 && this.carbonDioxide<50 && this.oxygen>=40 && this.oxygen<50)
+    	{
+    		this.atmosphere = 40;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 40% !");
+    	}
+    	if(this.carbonDioxide>=50 && this.carbonDioxide<60 && this.oxygen>=50 && this.oxygen<60)
+    	{
+    		this.atmosphere = 50;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 50% !");
+    	}
+    	if(this.carbonDioxide>=60 && this.carbonDioxide<70 && this.oxygen>=60 && this.oxygen<70)
+    	{
+    		this.atmosphere = 60;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 60% !");
+    	}
+    	if(this.carbonDioxide>=70 && this.carbonDioxide<80 && this.oxygen>=70 && this.oxygen()<80)
+    	{
+    		this.atmosphere = 70;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 70% !");
+    	}
+    	if(this.carbonDioxide>=80 && this.carbonDioxide<90 && this.oxygen>=80 && this.oxygen<90)
+    	{
+    		this.atmosphere = 80;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 80% !");
+    	}
+    	if(this.carbonDioxide>=90 && this.carbonDioxide<100 && this.oxygen>=90 && this.oygen<100)
+    	{
+    		this.atmosphere = 90;
+    		JOptionPane.showMessageDialog(null,"Atmosphère formée à 90% !");
+    	}
+    	if(this.carbonDioxide==80 && this.oxygen==100)
+    	{
+    		this.atmosphere = 100;
+    		JOptionPane.showMessageDialog(null,"Atmosphère complètement formée !");
     	}
     }
-    
-    /**
-     * @param None
-     * @brief Si les jauges d'O2 et de Co2 sont à 20% (individuellement) alors la jauge d'atmosphère passe à 20%
-     */
-    
-    public void atmosphereFirstAdvanced()
-    {
-    	double newOxygen;
-    	newOxygen = this.oxygen + vegetal_p.getOxygenGiven();
-    	this.setOxygen(newOxygen);
-    }
+}   
+
     
     
--- src/game/Game/Main.java
+++ src/game/Game/Main.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Game.species;
 
 public class Main {
 
@@ -11,9 +12,17 @@ public class Main {
 
 		Game notreJeu = new Game();
 		
-		Fish poisson = new Fish(carbonDioxideFish,strenght,typeOfAnimals);
+		Fish poisson = new Fish();
+		Tree arbre = new Tree();
 	
+		notreJeu.animalSpeciesIntroduced(poisson);
+		notreJeu.atmosphereStateUpdate();
 		
+		notreJeu.vegetalSpeciesIntroduced(poisson);
+		notreJeu.atmosphereStateUpdate();
+		
+		System.out.print("La jauge de CO2 est à " + notreJeu.getCarbonDioxide() + "%");
+		System.out.print("La jauge de 02 est à " + notreJeu.getOxygen() + "%");
 	}
 
 }
