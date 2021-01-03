1bbed60c8502ebc7b7a52a66ea91cec44ddeb711
Valentin Mercy <mercy.valentin@gmail.com>
2021-01-04 00:07:39 +0100

    added Missions class

---
+++ src/game/game/Missions.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Missions {
+
+    /**
+     * @param index_p index of mission to evaluate
+     * @return true if the mission is Achieved
+     */
+  public boolean isAchieved(int index_p)
+  {
+
+      return true;
+  }
+  
+}
