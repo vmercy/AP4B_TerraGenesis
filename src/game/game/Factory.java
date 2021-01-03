8003811a14e76111a3cb3e4d1cfdd0e23270a19c
Valentin Mercy <mercy.valentin@gmail.com>
2021-01-04 00:38:23 +0100

    added Factory class

---
+++ src/game/game/Factory.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Factory {
+  private byte biomassGain;
+
+  /**
+   * 
+   * @return biomassGain
+   */
+  public byte getBiomassGain()
+  {
+    return biomassGain;
+  }
+
+}
