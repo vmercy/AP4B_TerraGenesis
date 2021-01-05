2559c99433ecd670ff045fad6309ebb168500883
Valentin Mercy <mercy.valentin@gmail.com>
2021-01-05 15:58:38 +0100

    added Money class

---
+++ src/game/game/BuildingMaterial.java
@@ -0,0 +1,17 @@
+package game;
+
+public class BuildingMaterial {
+  private String m_name;
+
+  private double m_price;
+
+  /**
+   * true if the material has been bought by the player
+   */
+  private boolean m_owned;
+
+  public boolean buy(Money currentMoney)
+  {
+    if(currentMoney.)
+  }
+}
---
+++ src/game/game/Money.java
@@ -0,0 +1,6 @@
+package game;
+
+public class Money {
+  private double m_amount;
+  private String m_currency;
+}
