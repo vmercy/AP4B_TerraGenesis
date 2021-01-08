9c97138198fe81d7faac6c614c8186b9ede3b7e5
ZackBNT <zakaria.bnnt@gmail.com>
2021-01-08 21:52:06 +0100

    Afficher la description de la mission au survol du titre sur l'interface

--- src/game/game/Interface/HomeInterface.java
+++ src/game/game/Interface/HomeInterface.java
@@ -161,6 +161,7 @@ public class HomeInterface {
     lblMissionIntro.setBounds(955, 12, 69, 11);
     frame.getContentPane().add(lblMissionIntro);
 
+    lblCurrentMission.setToolTipText("Description...");
     lblCurrentMission.setHorizontalAlignment(SwingConstants.CENTER);
     lblCurrentMission.setForeground(Color.WHITE);
     lblCurrentMission.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
